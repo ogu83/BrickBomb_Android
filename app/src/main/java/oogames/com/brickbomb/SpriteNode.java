4311e11e0dd4611c0157454ab17e9d8b13568bc9
OGU <OGU@5dcceb64-0435-3b46-ae4c-57f2068f6179>
2016-02-12 08:03:50 +0000

    Menu Buttons clickable

---
+++ app/src/main/java/oogames/com/brickbomb/SpriteNode.java
@@ -0,0 +1,32 @@
+package oogames.com.brickbomb;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+/**
+ * Created by oguzkoroglu on 12/02/16.
+ */
+public class SpriteNode {
+    public Bitmap image;
+    public int X, Y;
+    public int Width;
+    public int Height;
+
+    public SpriteNode(Bitmap res, int w, int h) {
+        Width = w;
+        Height = h;
+        image = Bitmap.createScaledBitmap(res, w, h, false);
+    }
+
+    public boolean ContainsCGPosition(float x, float y) {
+        return x > X && x < X + Width && y > Y && y < Y + Height;
+    }
+
+    public void update() {
+
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(image, X, Y, null);
+    }
+}
