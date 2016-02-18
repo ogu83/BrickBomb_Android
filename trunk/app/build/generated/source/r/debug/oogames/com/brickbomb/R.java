e965737cae98640476ba5e808757418b759a5412
OGU <OGU@5dcceb64-0435-3b46-ae4c-57f2068f6179>
2016-02-18 23:30:18 +0000

    Game APK 1.5 Released Background Picture

--- trunk/app/build/generated/source/r/debug/oogames/com/brickbomb/R.java
+++ trunk/app/build/generated/source/r/debug/oogames/com/brickbomb/R.java
@@ -2207,20 +2207,21 @@ containing a value of this type.
         public static final int icon1=0x7f02004f;
         public static final int icon2=0x7f020050;
         public static final int icon3=0x7f020051;
-        public static final int launchimage=0x7f020052;
-        public static final int levelup=0x7f020053;
-        public static final int notification_template_icon_bg=0x7f02005f;
-        public static final int orangebrick=0x7f020054;
-        public static final int pause=0x7f020055;
-        public static final int play=0x7f020056;
-        public static final int purplebrick=0x7f020057;
-        public static final int rate=0x7f020058;
-        public static final int redbrick=0x7f020059;
-        public static final int soundoff=0x7f02005a;
-        public static final int soundon=0x7f02005b;
-        public static final int start=0x7f02005c;
-        public static final int tbrick=0x7f02005d;
-        public static final int yellowbrick=0x7f02005e;
+        public static final int invertedlightclouds=0x7f020052;
+        public static final int launchimage=0x7f020053;
+        public static final int levelup=0x7f020054;
+        public static final int notification_template_icon_bg=0x7f020060;
+        public static final int orangebrick=0x7f020055;
+        public static final int pause=0x7f020056;
+        public static final int play=0x7f020057;
+        public static final int purplebrick=0x7f020058;
+        public static final int rate=0x7f020059;
+        public static final int redbrick=0x7f02005a;
+        public static final int soundoff=0x7f02005b;
+        public static final int soundon=0x7f02005c;
+        public static final int start=0x7f02005d;
+        public static final int tbrick=0x7f02005e;
+        public static final int yellowbrick=0x7f02005f;
     }
     public static final class id {
         public static final int action0=0x7f0d0072;
